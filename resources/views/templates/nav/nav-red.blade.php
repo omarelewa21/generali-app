209862c01b2eb0730f4dc45e1ed6503a5cebc530
zeno-netis <netiswaran.thamilchelvam@zenomalaysia.com>
2023-07-03 18:36:51 +0800

    removed vue

--- resources/views/templates/nav/nav-red.blade.php
+++ resources/views/templates/nav/nav-red.blade.php
@@ -1,5 +1,5 @@
-<nav class="navbar navbar-default ms-5">
-    <div class="container">
+<nav class="navbar navbar-default">
+    <div class="container ps-5">
         <div class="navbar-header">
             <a class="navbar-brand" href="/">
                 <img class="main-logo img-fluid" src="{{ asset('images/logo.png') }}" width="132px" height="107" alt="Logo" style="margin-top:4.563rem;">
