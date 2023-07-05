61a04d1cfdf99c87a878bdab0d9d3520912a6729
Chegaram <nishalni.chegaram@zenomalaysia.com>
2023-07-05 14:17:49 +0800

    made changes to default layout

--- resources/views/pages/main/welcome.blade.php
+++ resources/views/pages/main/welcome.blade.php
@@ -1,3 +1,9 @@
+<?php
+ /**
+ * Template Name: Welcome Page
+ */
+?>
+
 @extends('templates.master')
 
 @section('title')
@@ -8,19 +14,23 @@
 
 @include('templates.nav.nav-red')
 
-<section id="home">
-    <div class="container px-5">
-        <div class="row">
-            <div class="col-xxl-6">
-                <h1 class="text-uppercase text-dark">Welcome!</h1>
-                <h2 class="text-uppercase text-dark">Your Future Awaits.</h2>
-                <p class="text-dark py-4">We’re glad you’re looking to secure your future with us.<br>
-                    Let’s begin by getting to know you
-                    better.</p>
-                <a href="{{ route('pdpa.disclosure') }}" class="btn btn-primary py-3">START YOUR JOURNEY</a>
-
+<div id="home">
+    <section class="main-content">
+        <div class="container px-5">
+            <div class="row">
+                <div class="col-6">
+                    <h1 class="text-uppercase text-dark">Welcome!</h1>
+                    <h2 class="text-uppercase text-dark">Your Future Awaits.</h2>
+                    <p class="text-dark py-4">We’re glad you’re looking to secure your future with us.<br>
+                        Let’s begin by getting to know you
+                        better.</p>
+                    <a href="{{ route('pdpa.disclosure') }}" class="btn btn-primary py-3">START YOUR JOURNEY</a>
+                </div>
             </div>
-
-    </div>
-</section>
+        </div>
+    </section>
+    <section class="footer position-absolute bottom-0 start-0">
+        <img src="{{ asset('images/welcome-page/home-vector.png') }}" width="30%" alt="Footer Vector">
+    </section>
+</div>
 @endsection
