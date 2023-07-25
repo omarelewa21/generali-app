4b66d507ea26a9d4320d248eea16348947ad80c4
NishalniChegaram <137253638+NishalniChegaram@users.noreply.github.com>
2023-07-25 18:54:21 +0800

    added navbar

--- resources/views/templates/nav/nav-red-menu.blade.php
+++ resources/views/templates/nav/nav-red-menu.blade.php
@@ -9,11 +9,10 @@
 {{--end of nav links --}}
 
 <header id="wrapper-navbar">
-    <nav>
-        <div class="container px-5 pt-4 pt-sm-4 pt-md-5 pb-4 pb-sm-4 pb-md-3">
+    <nav class="navbar navbar-default transparent">
+        <div class="container-fluid px-5 pt-4 pt-sm-4 pt-md-5 pb-4 pb-sm-4 pb-md-3">
             <a data-bs-toggle="offcanvas" href="#offcanvasMenu" role="button" aria-controls="offcanvasMenu">
-                <img class="d-flex" src="{{ asset('images/menu-button-red.svg') }}" alt="Logo" width="32px"
-                    height="26px">
+                <img class="d-flex" src="{{ asset('images/menu-button-red.svg') }}" alt="Logo" width="32px" height="26px">
             </a>
         </div>
     </nav>
