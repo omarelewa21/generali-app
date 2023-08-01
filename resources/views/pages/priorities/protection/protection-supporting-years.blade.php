48f71ee798e11e99057ecae09a493948690764cf
zeno-netis <netiswaran.thamilchelvam@zenomalaysia.com>
2023-08-01 09:36:59 +0800

    made some changes

--- resources/views/pages/priorities/protection/protection-supporting-years.blade.php
+++ resources/views/pages/priorities/protection/protection-supporting-years.blade.php
@@ -8,25 +8,25 @@
 <div id="investment-supporting">
     <div class="container-fluid overflow-hidden font-color-default text-center">
         <div class="row bg-investment-supporting vh-100">
-            <section class="col-12 d-flex needs-nav-mob">
-                <div class="col-2 col-md-2 col-xl-3 sticky-top">
+            <div class="row">
+                <div class="col-3 col-md-3 col-lg-3">
                     @include('templates.nav.nav-red-menu')
                 </div>
-                <div class="col-md-7 col-xl-6 hide-mobile">
+                <div class="col-6 col-md-6 col-lg-6">
                     <div class="row d-flex justify-content-center align-items-center">
-                        <div class="col-6 bg-primary container fund-nav-rad">
-                            <div class="col-12 fund-progress mt-4 d-flex justify-content-enter align-items-center">
-                                <div class="px-2 fund-progress-bar" style="width:75%;"></div>
-                            </div>
-                            <h3 class="font-color-white">RM1,500,000</h3>
-                            <p class="font-color-white">Total Protection Fund Needed</p>
+                        <div class="col-lg-6 col-sm-12 bg-primary summary-progress-bar px-1">
+                                <div class="col-12 retirement-progress mt-3 d-flex justify-content-enter align-items-center">
+                                    <div class="px-2 retirement-progress-bar" role="progressbar" style="width:45%;" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                                <h3 class="needsProgressValue m-1 text-light text-center">RM1,500,000</h3>
+                                <p class="text-light text-center">Total Protection Fund Needed</p>
                         </div>
                     </div>
                 </div>
-                <div class="col-10 col-md-3 col-xl-3 hide">
+                <div class="col-3 col-md-3 col-lg-3">
                     @include ('templates.nav.nav-sidebar-needs')
-                </div>
-            </section>
+                </div>  
+            </div>
             <form class="form-horizontal p-0" action="{{route('protection.existing.policy')}}" method="get" id="investment" name="investment">
                 <section class="needs-master-content">
                     <div class="col-12">
