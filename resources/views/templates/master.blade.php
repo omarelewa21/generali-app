47e94b9564256e76afaa42a035ce99ea871e3b1e
Ying43 <127811446+Ying43@users.noreply.github.com>
2023-08-07 18:28:04 +0800

    completed the left side menu

--- resources/views/templates/master.blade.php
+++ resources/views/templates/master.blade.php
@@ -20,7 +20,8 @@
     <link href="{{ asset('css/styles.css') }}" rel="stylesheet" type="text/css">
     <link href="{{ asset('css/custom-ying.css') }}" rel="stylesheet" type="text/css">
     <link href="{{ asset('css/netis-styles.css') }}" rel="stylesheet" type="text/css">
-    
+    <link href="{{ asset('css/slick.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/slick-theme.min.css') }}" rel="stylesheet" type="text/css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     {{-- @vite(['resources/js/custom-ying.js']) --}}
     @vite(['resources/js/jquery.min.js'])
