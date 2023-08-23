7de30682849f973f7b5ef056857302d94182568f
Ying43 <127811446+Ying43@users.noreply.github.com>
2023-08-23 15:49:30 +0800

    created tables database

--- database/migrations/2023_08_23_074619_create_marital_statuses_table.php
+++ database/migrations/2023_08_23_074619_create_marital_statuses_table.php
@@ -11,9 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('countries', function (Blueprint $table) {
+        Schema::create('marital_statuses', function (Blueprint $table) {
             $table->id();
-            $table->string('countries');
             $table->timestamps();
         });
     }
@@ -23,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('countries');
+        Schema::dropIfExists('marital_statuses');
     }
 };
