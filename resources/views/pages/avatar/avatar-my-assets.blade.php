73232aef6a43d7aee540d3b9877672c5e709d377
NishalniChegaram <137253638+NishalniChegaram@users.noreply.github.com>
2023-08-24 10:12:08 +0800

    added modal for assets page

--- resources/views/pages/avatar/avatar-my-assets.blade.php
+++ resources/views/pages/avatar/avatar-my-assets.blade.php
@@ -12,8 +12,6 @@
 
 @section('content')
 
-@include('templates.nav.nav-red-menu')
-
 @php
     // Retrieving values from the session
     $arrayData = session('passingArrays');
@@ -25,6 +23,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-12 col-md-6 col-lg-6 col-xxl-7 col-xl-7 gender-selection-bg vh-100 wrapper-avatar-default assets-overlay overflow-hidden px-0">
+                <div class="header-avatar-default">@include('templates.nav.nav-red-menu')</div>
                 <section class="avatar-design-placeholder content-avatar-default position-relative imageContainerHouse"></section>
                 <section class="footer-avatar-default d-flex justify-content-center">
                     <div class="col-12 position-relative imageContainerCar"></div>
@@ -44,7 +43,7 @@
                             <div class="row px-4 pb-4 px-sm-5">
                                 <div class="col-12 col-xxl-6 col-xl-6 col-lg-12 col-md-12 col-sm-6 text-dark fade-effect pt-2 pb-3">
                                     <div class="col-12 button-bg {{$assets === 'car' ? 'selected' : ''}}">
-                                        <div class="col-12 py-4 d-flex align-items-center justify-content-center hover">
+                                        <div class="col-12 py-4 d-flex align-items-center justify-content-center hover border-default">
                                             <button class="border-0" data-avatar="car" data-required="" id="carButton">
                                                 <img src="{{ asset('images/avatar-my-assets/car-icon-02.png') }}" width="auto" height="100px" alt="Car">
                                                 <p class="avatar-text text-center pt-4 mb-0 fw-bold">Car</p>
@@ -54,7 +53,7 @@
                                 </div>
                                 <div class="col-12 col-xxl-6 col-xl-6 col-lg-12 col-md-12 col-sm-6 text-dark fade-effect pt-2 pb-3">
                                     <div class="col-12 button-bg {{$assets === 'motorcycle' ? 'selected' : ''}}">
-                                        <div class="col-12 py-4 d-flex align-items-center justify-content-center hover">
+                                        <div class="col-12 py-4 d-flex align-items-center justify-content-center hover border-default">
                                             <button class="border-0" data-avatar="motorcycle" data-required="" id="scooterButton">
                                                 <img src="{{ asset('images/avatar-my-assets/motorcycle-icon.png') }}" width="auto" height="100px" alt="Motorcycle">
                                                 <p class="avatar-text text-center pt-4 mb-0 fw-bold">Motorcycle</p>
@@ -64,7 +63,7 @@
                                 </div>
                                 <div class="col-12 col-xxl-6 col-xl-6 col-lg-12 col-md-12 col-sm-6 text-dark fade-effect pt-2 pb-3">
                                     <div class="col-12 button-bg {{$assets === 'house' ? 'selected' : ''}}">
-                                        <div class="col-12 py-4 d-flex align-items-center justify-content-center hover">
+                                        <div class="col-12 py-4 d-flex align-items-center justify-content-center hover border-default">
                                             <button class="border-0" data-avatar="house" data-required="" id="houseButton">
                                                 <img src="{{ asset('images/avatar-my-assets/house-icon.png') }}" width="auto" height="100px" alt="House">
                                                 <p class="avatar-text text-center pt-4 mb-0 fw-bold">House</p>
@@ -74,8 +73,8 @@
                                 </div>
                                 <div class="col-12 col-xxl-6 col-xl-6 col-lg-12 col-md-12 col-sm-6 text-dark fade-effect pt-2 pb-3">
                                     <div class="col-12 button-bg {{$assets === 'bungalow' ? 'selected' : ''}}">
-                                        <div class="col-12 py-4 d-flex align-items-center justify-content-center hover">
-                                            <button class="border-0" data-avatar="bungalow" data-required="">
+                                        <div class="col-12 py-4 d-flex align-items-center justify-content-center hover border-default">
+                                            <button class="border-0" data-avatar="bungalow" data-required="" id="bungalowButton">
                                                 <img src="{{ asset('images/avatar-my-assets/bungalow-icon.png') }}" width="auto" height="100px" alt="Bungalow">
                                                 <p class="avatar-text text-center pt-4 mb-0 fw-bold">Bungalow</p>
                                             </button>
@@ -84,8 +83,8 @@
                                 </div>
                                 <div class="col-12 col-xxl-6 col-xl-6 col-lg-12 col-md-12 col-sm-6 text-dark fade-effect pt-2 pb-3">
                                     <div class="col-12 button-bg {{$assets === 'apartment' ? 'selected' : ''}}">
-                                        <div class="col-12 py-4 d-flex align-items-center justify-content-center hover">
-                                            <button class="border-0" data-avatar="apartment" data-required="">
+                                        <div class="col-12 py-4 d-flex align-items-center justify-content-center hover border-default">
+                                            <button class="border-0" data-avatar="apartment" data-required="" id="condoButton">
                                                 <img src="{{ asset('images/avatar-my-assets/apartment-icon.png') }}" width="auto" height="100px" alt="Apartment">
                                                 <p class="avatar-text text-center pt-4 mb-0 fw-bold">Apartment</p>
                                             </button>
@@ -94,8 +93,8 @@
                                 </div>
                                 <div class="col-12 col-xxl-6 col-xl-6 col-lg-12 col-md-12 col-sm-6 text-dark fade-effect pt-2 pb-3">
                                     <div class="col-12 button-bg {{$assets === 'others' ? 'selected' : ''}}">
-                                        <div class="col-12 py-4 d-flex align-items-center justify-content-center hover">
-                                            <button class="border-0" data-avatar="others" data-required="">
+                                        <div class="col-12 py-4 d-flex align-items-center justify-content-center hover border-default">
+                                            <button class="border-0" data-avatar="others" data-required="" data-bs-toggle="modal" data-bs-target="#otherAssets">
                                                 <img src="{{ asset('images/avatar-my-assets/others-icon.png') }}" width="auto" height="100px" alt="Others">
                                                 <p class="avatar-text text-center pt-4 mb-0 fw-bold">Others</p>
                                             </button>
@@ -117,6 +116,26 @@
                         </div>
                     </section>
                 </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="otherAssets" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="otherAssetsLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="d-flex justify-content-end px-3 py-3">
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-header px-5 pt-2 pb-0">
+                <h3 class="modal-title text-center text-uppercase otherModalText" id="otherAssetsLabel">I Have</h2>
+            </div>
+            <div class="modal-body text-white text-center px-5 pt-5 bg-primary">
+                <input type="text" name="otherAssetsInput" class="form-control bg-white @error('otherAssetsInput') is-invalid @enderror" id="otherAssetsInput" placeholder="Add your asset" value="{{ old('otherAssetsInput', $arrayData['otherAssetsInput'] ?? '') }}">
+            </div>
+            <div class="modal-footer border-0">
+                <button type="button" class="btn btn-outline-secondary text-uppercase btn-exit-sidebar" data-bs-dismiss="modal">Submit</button>
             </div>
         </div>
     </div>
