8dcdcb3848e41f3467aa342f37f3f510473a0779
Ying43 <127811446+Ying43@users.noreply.github.com>
2023-08-24 10:57:33 +0800

    updated education home

--- resources/views/pages/priorities/education/education-home.blade.php
+++ resources/views/pages/priorities/education/education-home.blade.php
@@ -16,25 +16,30 @@
             </div>
             <div class="col-12 col-md-9 px-0 bg-education-home d-vh-100">
                 <hr class="py-1 m-0 bg-primary opacity-100 border-0 d-none d-md-block" />
-                <section class="needs-home">
-                    <div class="content-needs">
+                <section class="education-needs-home">
+                    <div class="row">
+                    </div>
+                    <div class="bg-education-book">
+                        <!-- <img class="z-1 d-flex justify-content-center align-items-center m-auto position-absolute center w-75" src="{{ asset('images/needs/education/education-plain-home.png') }}" alt="Education Background" style=""> -->
+                        <div class="education-content-needs">
                             <div class="needs-home-avatar col-12 text-center d-flex flex-column justify-content-start justify-content-md-center justify-content-lg-center align-items-center py-2 position-relative">
-                                <img class="z-1" src="{{ asset('images/needs/education/education-plain-home.png') }}" alt="Education Background" style="width:85%;">
-                                <img class="z-1 position-absolute center up" src="{{ asset('images/needs/education/education-avatar.png') }}" alt="Education Avatar">
+                                <!-- <img class="z-1" src="{{ asset('images/needs/education/education-plain-home.png') }}" alt="Education Background" style="">  -->
+                                <img class="z-1" src="{{ asset('images/needs/education/education-avatar.png') }}" alt="Education Avatar">
                                 <h5 class="z-1 d-flex col-12 col-md-8 justify-content-center needs-grey-bg-mobile">Let's get into your plans for Education.</h5>
                             </div>
                             <div class="d-flex needs-grey-bg justify-content-center position-absolute w-100 bottom-0">
                                 <div class="col-11 col-md-4 text-center">
                                 </div>
                             </div> 
+                        </div>
                     </div>
                 </section>
                 <section class="footer bg-white py-4 fixed-bottom">
                     <div class="container-fluid">
                         <div class="row">
                             <div class="col-12 d-flex gap-2 d-md-block text-end px-4">
-                                <a href="{{route('retirement.allocated.funds.aside')}}" class="btn btn-primary text-uppercase flex-fill me-md-2">Back</a>
-                                <a href="{{route('education.coverage')}}" class="btn btn-primary text-uppercase flex-fill">Next</a>
+                                <a href="{{route('retirement.allocated.funds.aside')}}" class="btn btn-primary flex-fill me-md-2 text-uppercase">Back</a>
+                                <a href="{{route('education.coverage')}}" class="btn btn-primary flex-fill text-uppercase">Next</a>
                             </div>
                         </div>
                     </div>
@@ -45,3 +50,15 @@
 </div>
 
 @endsection 
+
+<!-- <div class="content-needs">
+                        <div class="needs-home-avatar col-12 text-center d-flex flex-column justify-content-start justify-content-md-center justify-content-lg-center align-items-center py-2 position-relative">
+                             <img class="z-1" src="{{ asset('images/needs/education/education-plain-home.png') }}" alt="Education Background" style=""> 
+                            <img class="z-1" src="{{ asset('images/needs/education/education-avatar.png') }}" alt="Education Avatar">
+                            <h5 class="z-1 d-flex col-12 col-md-8 justify-content-center needs-grey-bg-mobile">Let's get into your plans for Education.</h5>
+                        </div>
+                        <div class="d-flex needs-grey-bg justify-content-center position-absolute w-100 bottom-0">
+                            <div class="col-11 col-md-4 text-center">
+                            </div>
+                        </div> 
+                    </div> -->
