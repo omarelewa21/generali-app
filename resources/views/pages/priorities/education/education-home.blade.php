9492ce2fb2a2daf02aa594a5b4ae138cab079f32
Ying43 <127811446+Ying43@users.noreply.github.com>
2023-09-08 18:52:15 +0800

    done all the pages for education needs except the limitation min bug for money haven't fix and waiting creative team to update all the new design

--- resources/views/pages/priorities/education/education-home.blade.php
+++ resources/views/pages/priorities/education/education-home.blade.php
@@ -10,60 +10,76 @@
 
 @section('content')
 
-<div id="education-home" class="vh-100">
+<div id="education-home" class="vh-100 overflow-x-hidden scrollable-content">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-12 col-md-2 bg-primary sidebanner">
+            <div class="col-12 col-md-3 col-xl-2 bg-primary sidebanner">
                 @include('templates.nav.nav-white-menu')
-                <div class="text-white px-4 px-md-5 py-md-5 py-3">
-                    <h2 class="display-5 font-bold fw-bold">My Priorities</h2>
+                <div class="text-white px-4 px-xl-5 py-3 py-md-5">
+                    <h4 class="display-5 fw-bold text-white">My Priorities</h4>
                 </div>
             </div>
-            <div class="col-12 col-md-9 px-0 bg-education-home d-vh-100">
-                <hr class="py-1 m-0 bg-primary opacity-100 border-0 d-none d-md-block" />
-                <section class="education-needs-home">
-                    <div class="row">
-                    </div>
-                    <div class="bg-education-book">
-                        <!-- <img class="z-1 d-flex justify-content-center align-items-center m-auto position-absolute center w-75" src="{{ asset('images/needs/education/education-plain-home.png') }}" alt="Education Background" style=""> -->
-                        <div class="education-content-needs">
-                            <div class="needs-home-avatar col-12 text-center d-flex flex-column justify-content-start justify-content-md-center justify-content-lg-center align-items-center py-2 position-relative">
-                                <!-- <img class="z-1" src="{{ asset('images/needs/education/education-plain-home.png') }}" alt="Education Background" style="">  -->
-                                <img class="z-1" src="{{ asset('images/needs/education/education-avatar.png') }}" alt="Education Avatar">
-                                <h5 class="z-1 d-flex col-12 col-md-8 justify-content-center needs-grey-bg-mobile">Let's get into your plans for Education.</h5>
-                            </div>
-                            <div class="d-flex needs-grey-bg justify-content-center position-absolute w-100 bottom-0">
-                                <div class="col-11 col-md-4 text-center">
+            <div class="col-12 col-md-9 col-xl-10 bg-accent-bg-grey text-dark px-0">
+                <div class="vh-md-100 overflow-y-auto overflow-x-hidden">
+                    <hr class="py-1 m-0 bg-primary opacity-100 border-0 d-none d-md-block" />
+                    <section class="needs-home-wrapper bg-needs-home">
+                        <div class="container needs-home-contents bg-needs-home-element">
+                            <div class="row needs-home-avatar-wrapper justify-content-center h-100">
+                                <div class="col-12 needs-home-avatars z-1 align-items-end">
+                                    <div class="position-relative h-100 mh-100">
+                                        <!-- <img src="{{ asset('images/needs/education/home/books-left.png') }}" class="position-absolute bottom-0 end-0 mh-90"> -->
+                                    </div>
+                                    <div class="position-relative h-100 mh-100 z-1 d-flex justify-content-center">
+                                        <img src="{{ asset('images/needs/education/home/books-left.png') }}" class="position-absolute bottom-0 start-25 mh-90">
+                                        <img src="{{ asset('images/needs/education/home/home-avatar.png') }}" class="position-absolute bottom-0 mh-90 z-1 start-20">
+                                        <img src="{{ asset('images/needs/education/home/books-right.png') }}" class="position-absolute bottom-0 end-0 mh-90">
+                                    </div>
+                                    <div class="position-relative h-100 mh-100">
+                                        <!-- <img src="{{ asset('images/needs/education/home/books-right.png') }}" class="position-absolute bottom-0 start-0 mh-90"> -->
+                                    </div>
                                 </div>
-                            </div> 
+                                <div class="col-12 needs-home-text z-1 align-items-end d-grid">
+                                    <div class="col-md-6 d-flex justify-content-center text-center m-auto z-1">
+                                        <h4 class="py-3 f-family fw-700">Let's get into your plans for education</h4>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </section>
-                <section class="footer bg-white py-4 fixed-bottom">
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-12 d-flex gap-2 d-md-block text-end px-4">
-                                <a href="{{route('retirement.allocated.funds.aside')}}" class="btn btn-primary flex-fill me-md-2 text-uppercase">Back</a>
-                                <a href="{{route('education.coverage.new')}}" class="btn btn-primary flex-fill text-uppercase">Next</a>
+                    </section>
+                    <div class="col-12 show-mobile footer">
+                        <div class="container-fluid">
+                            <div class="row">
+                                <div class="col-12 d-flex gap-2 d-md-block text-end px-4 z-1 bg-white py-4">
+                                    <a href="{{route('education.coverage.new')}}" class="btn btn-primary flex-fill me-md-2 text-uppercase">Back</a>
+                                    <button type="submit" class="btn btn-primary flex-fill text-uppercase" id="nextButton">Next</button>
+                                </div>
+                                <div class="col-12 position-absolute bottom-0 show-mobile">
+                                    <div class="row">
+                                        <div class="needs-stand-bg bg-btn_bar"></div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </section>
+                    <div class="col-12 hide-mobile">
+                        <div class="row">
+                            <div class="position-absolute bg-btn_bar bottom-0 needs-stand-bg"></div>
+                        </div>
+                    </div>
+                    <section class="footer bg-white py-4 fixed-bottom hide-mobile">
+                        <div class="container-fluid">
+                            <div class="row">
+                                <div class="col-12 d-flex gap-2 d-md-block text-end px-4">
+                                    <a href="{{route('retirement.allocated.funds.aside')}}" class="btn btn-primary flex-fill me-md-2 text-uppercase">Back</a>
+                                    <a href="{{route('education.coverage.new')}}" class="btn btn-primary flex-fill text-uppercase">Next</a>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
             </div>
         </div>
     </div>
 </div>
 
 @endsection 
-
-<!-- <div class="content-needs">
-                        <div class="needs-home-avatar col-12 text-center d-flex flex-column justify-content-start justify-content-md-center justify-content-lg-center align-items-center py-2 position-relative">
-                             <img class="z-1" src="{{ asset('images/needs/education/education-plain-home.png') }}" alt="Education Background" style=""> 
-                            <img class="z-1" src="{{ asset('images/needs/education/education-avatar.png') }}" alt="Education Avatar">
-                            <h5 class="z-1 d-flex col-12 col-md-8 justify-content-center needs-grey-bg-mobile">Let's get into your plans for Education.</h5>
-                        </div>
-                        <div class="d-flex needs-grey-bg justify-content-center position-absolute w-100 bottom-0">
-                            <div class="col-11 col-md-4 text-center">
-                            </div>
-                        </div> 
-                    </div> -->
