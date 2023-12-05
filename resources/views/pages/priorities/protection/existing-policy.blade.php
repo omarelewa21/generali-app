ad56c3b59c428f46de5c3a3ebb8e21cd8ac69fde
Ying43 <127811446+Ying43@users.noreply.github.com>
2023-12-05 18:34:43 +0800

    updated new changes - added new data to store in backend

--- resources/views/pages/priorities/protection/existing-policy.blade.php
+++ resources/views/pages/priorities/protection/existing-policy.blade.php
@@ -14,6 +14,7 @@
 
 @php
     // Retrieving values from the session
+    $protectionPriority = session('customer_details.priorities.protectionDiscuss');
     $existingPolicy = session('customer_details.protection_needs.existingPolicy');
     $existingPolicyAmount = session('customer_details.protection_needs.existingPolicyAmount');
     $totalProtectionNeeded = session('customer_details.protection_needs.totalProtectionNeeded');
@@ -99,10 +100,26 @@
         </div>
     </div>
 </div>
+<div class="modal fade" id="missingProtectionFields" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header px-4 pt-4 justify-content-center">
+                <h3 class="modal-title fs-4 text-center" id="missingProtectionFieldsLabel">Protection Priority to discuss is required.</h2>
+            </div>
+            <div class="modal-body text-dark text-center px-4 pb-4">
+                <p>Please click proceed to enable protection priority to discuss in Priorities To Discuss page first.</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-outline-secondary text-uppercase btn-exit-sidebar" data-bs-dismiss="modal">Proceed</button>
+            </div>
+        </div>
+    </div>
+</div>
 
 <script>
     var oldTotalFund = parseFloat({{ $totalProtectionNeeded }});
     var protectionFundPercentage = parseFloat({{ $protectionFundPercentage }});
     var sessionExistingPolicyAmount = parseFloat({{$existingPolicyAmount}});
+    var protectionPriority = {{$protectionPriority}};
 </script>
 @endsection
