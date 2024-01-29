e4954fdbd6fbfd0075a1887e1a5dfb797561d23f
NishalniChegaram <nishalni.chegaram@zenogroup.com>
2024-01-29 16:52:07 +0800

    changes

--- resources/views/templates/nav/nav-links-desktop.blade.php
+++ resources/views/templates/nav/nav-links-desktop.blade.php
@@ -56,10 +56,10 @@
     <div class="footer-navigation col-12">
         <div class="col-12 px-5 py-4 inline-block">
             <!-- Button trigger modal -->
-            <a href="{{route('welcome')}}" class="btn btn-outline-secondary btn-exit text-uppercase pb-2" type="button" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+            <a href="{{route('clear_session_storage')}}" class="btn btn-outline-secondary btn-exit text-uppercase pb-2" type="button" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
             Save & Logout</a>
-            <a href="{{route('clear_session_storage')}}" class="btn btn-outline-secondary btn-exit text-uppercase pb-2" id="clearSession" data-clear-route="{{ route('clear_session_data') }}" type="button">
-                Clear Session</a>
+            <!-- <a href="{{route('clear_session_storage')}}" class="btn btn-outline-secondary btn-exit text-uppercase pb-2" id="clearSession" data-clear-route="{{ route('clear_session_data') }}" type="button">
+                Clear Session</a> -->
         </div>
     </div>
 </div>
